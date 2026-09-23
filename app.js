5101147e49c44a50b6848eada2b4abcdca690d36
Mukhty <mabdulfattah497@gmail.com>
2026-09-23 07:59:42 +0100

    made a change

--- app.js
+++ app.js
@@ -546,6 +546,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!str) return '';
     return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
   }
+  
 
   window.copyCardLink = function(url, btn) {
     navigator.clipboard.writeText(url).then(() => {
